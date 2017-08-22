01580f84d48f118c3533cf8d225c4201cbcd86d5
boeckhoff <nico.boeckhoff@student.hpi.de>
2017-08-22 23:44:00 +0200

    modes and brightness (festival proof version)

--- src/stab.cpp
+++ src/stab.cpp
@@ -5,79 +5,6 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
-/* 
-	 This code should be pasted within the files where this function is needed.
-	 This function will not create any code conflicts.
-
-	 The function call is similar to printf: ardprintf("Test %d %s", 25, "string");
-
-	 To print the '%' character, use '%%'
-
-	 This code was first posted on http://arduino.stackexchange.com/a/201
-	 */
-
-#ifndef ARDPRINTF
-#define ARDPRINTF
-#define ARDBUFFER 16 //Buffer for storing intermediate strings. Performance may vary depending on size.
-#include <stdarg.h>
-
-int ardprintf(char *str, ...) //Variadic Function
-{
-	int i, count=0, j=0, flag=0;
-	char temp[ARDBUFFER+1];
-	for(i=0; str[i]!='\0';i++)  if(str[i]=='%')  count++; //Evaluate number of arguments required to be printed
-
-	va_list argv;
-	va_start(argv, count);
-	for(i=0,j=0; str[i]!='\0';i++) //Iterate over formatting string
-	{
-		if(str[i]=='%')
-		{
-			//Clear buffer
-			temp[j] = '\0'; 
-			Serial.print(temp);
-			j=0;
-			temp[0] = '\0';
-
-			//Process argument
-			switch(str[++i])
-			{
-				case 'd': Serial.print(va_arg(argv, int));
-									break;
-				case 'l': Serial.print(va_arg(argv, long));
-									break;
-				case 'f': Serial.print(va_arg(argv, double));
-									break;
-				case 'c': Serial.print((char)va_arg(argv, int));
-									break;
-				case 's': Serial.print(va_arg(argv, char *));
-									break;
-				default:  ;
-			};
-		}
-		else 
-		{
-			//Add to buffer
-			temp[j] = str[i];
-			j = (j+1)%ARDBUFFER;
-			if(j==0)  //If buffer is full, empty buffer.
-			{
-				temp[ARDBUFFER] = '\0';
-				Serial.print(temp);
-				temp[0]='\0';
-			}
-		}
-	};
-
-	Serial.println(); //Print trailing newline
-	return count + 1; //Return number of arguments detected
-}
-
-#undef ARDBUFFER
-#endif
-
-
-
 
 // Pins
 #define X_PIN A3
@@ -94,11 +21,11 @@ int ardprintf(char *str, ...) //Variadic Function
 #define RECORDING 1
 #define PLAYBACK 2
 
-#define maxBrightnessMode 3
-#define maxColorMode 1
+#define maxBrightnessMode 4
+#define maxColorMode 5
 
 // Parameters
-#define STEP_DELAY 20
+#define STEP_DELAY 5
 #define NUMPIXELS  50
 
 // Setup Led Strip
@@ -133,8 +60,8 @@ uint8_t brightnessMap[NUMPIXELS];
 // Initialize emtpy sequence
 uint8_t *sequenceEnd = sequenceMemory;
 
-uint8_t brightnessMode = 0;
-uint8_t colorMode = 0;
+int brightnessMode = 0;
+int colorMode = 0;
 
 int xDefPos;
 int yDefPos;
@@ -152,101 +79,97 @@ void setup() {
 	Serial.begin(9600);
 }
 
-uint8_t getCurrentPin() {
+struct RGB
+{
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+};
+
+struct HSL
+{
+	int H;
+	float S;
+	float L;
+};
+
+float HueToRGB(float v1, float v2, float vH)
+{
+	if (vH < 0)
+		vH += 1;
+
+	if (vH > 1)
+		vH -= 1;
+
+	if ((6 * vH) < 1)
+		return (v1 + (v2 - v1) * 6 * vH);
+
+	if ((2 * vH) < 1)
+		return v2;
+
+	if ((3 * vH) < 2)
+		return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+
+	return v1;
+}
+
+struct RGB HSLToRGB(struct HSL hsl) {
+	struct RGB rgb;
+
+	if (hsl.S == 0)
+	{
+		rgb.R = rgb.G = rgb.B = (unsigned char)(hsl.L * 255);
+	}
+	else
+	{
+		float v1, v2;
+		float hue = (float)hsl.H / 360;
+
+		v2 = (hsl.L < 0.5) ? (hsl.L * (1 + hsl.S)) : ((hsl.L + hsl.S) - (hsl.L * hsl.S));
+		v1 = 2 * hsl.L - v2;
+
+		rgb.R = (unsigned char)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+		rgb.G = (unsigned char)(255 * HueToRGB(v1, v2, hue));
+		rgb.B = (unsigned char)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+	}
+
+	return rgb;
+}
+
+uint8_t getCurrentPin(bool checkTreshold) {
 
 	int xOrig = analogRead(X_PIN);
 	int yOrig = analogRead(Y_PIN);
 
-	int xAdj = xOrig - xDefPos;
-	int yAdj = yOrig - yDefPos;
+	int xPosition = xOrig - 420;
+	int yPosition = yOrig - 420;
 
-	int diff = abs(xAdj) + abs(yAdj);
+	int diff = abs(xOrig - xDefPos) + abs(yOrig - yDefPos);
 	
-	if(diff < 20) {
-		//Serial.println("diff below threshold");
+	if(diff < 200 && checkTreshold) {
 		return INT8_MAX;
 	}
 
-	float xSquare = (xOrig - xDefPos)/425.0;
-	float ySquare = (yOrig - yDefPos)/425.0;
+	xPosition = xPosition;
+	yPosition = -yPosition;
 
-	ySquare = -ySquare;
-
-	if(xSquare > 1.0) {
-		xSquare = 1.0;
-	}
-
-	if(xSquare < -1.0) {
-		xSquare = -1.0;
-	}
-
-	if(ySquare > 1.0) {
-		ySquare = 1.0;
-	}
-
-	if(ySquare < -1.0) {
-		ySquare = -1.0;
-	}
-
-	//ySquare = -ySquare;
-
-	float xCircle = xSquare * sqrt(1.0 - 0.5*ySquare*ySquare);
-	float yCircle = ySquare * sqrt(1.0 - 0.5*xSquare*xSquare);
-
-	float param = (float)xCircle/(float)yCircle;
+	float param = (float)xPosition/(float)yPosition;
 	float rad = atan(param);
 	float deg = rad * (180.0 / PI);
 
-	if(yCircle > 0.01) {
+	if(yPosition > 0) {
 		deg = 90 - deg;
 	}
-	if(yCircle < -0.01) {
+	if(yPosition < 0) {
 		deg = 270 - deg;
 	}
 
 	float segment = 360.0/(float)NUMPIXELS;
 	uint8_t pin = (((uint8_t)(round(deg/segment))+3*(NUMPIXELS/4)+1) % NUMPIXELS);
-	ardprintf("XOrig %d YOrig %d XAdj %d yAdj %d XSquare %f YSquare %f XCircle %f YCircle %f diff %d degree %f segmentWidth %f Pin %d", xOrig, yOrig, (xOrig - xDefPos), (yOrig - yDefPos), xSquare, ySquare, xCircle, yCircle, diff, deg, segment, pin);
+	
+	//ardprintf("XPos %d YPos %d XPosAdj %d YPosAdj %d diff %d degree %f segmentWidth %f Pin %d", xOrig, yOrig, xPosition, yPosition, diff, deg, segment, pin);
 
 	return pin;
-}
-
-void setColorMapBasedOnPin(uint8_t pin) {
-
-	switch(colorMode) {
-		case 0:
-			//red
-			for(uint8_t i=0; i<NUMPIXELS; ++i) {
-				colorMap[i][0] = 1;
-				colorMap[i][1] = 0;
-				colorMap[i][2] = 0;
-			}
-			break;
-		case 1:
-			//red green blue in succession
-			for(uint8_t i=0; i<NUMPIXELS; ++i) {
-				uint8_t diff = pin%3;
-				switch(diff) {
-					case 0:
-						colorMap[i][0] = 1;
-						colorMap[i][1] = 0;
-						colorMap[i][2] = 0;
-						break;
-					case 1:
-						colorMap[i][0] = 0;
-						colorMap[i][1] = 1;
-						colorMap[i][2] = 0;
-						break;
-					case 2:
-						colorMap[i][0] = 0;
-						colorMap[i][1] = 0;
-						colorMap[i][2] = 1;
-						break;
-				}
-			}
-			break;
-		//adjust maxColorMode when adding modes
-	}
 }
 
 uint8_t distanceToPin(uint8_t first, uint8_t second) {
@@ -255,62 +178,138 @@ uint8_t distanceToPin(uint8_t first, uint8_t second) {
 			abs(second - first));
 }
 
-void checkModeButton() {
-	modeButtonStatus = digitalRead(MODE_BUTTON_PIN);
-
-	if(modeButtonStatus == LOW && prevModeButtonStatus == HIGH) {
-		prevModeButtonStatus = LOW;
-		uint8_t curPin = getCurrentPin();
-
-		Serial.println("Current Pin");
-		Serial.println(getCurrentPin());
-
-		if(curPin >= 33 && curPin <= 39) { 
-			// right - next brightness mode
-
-			if(brightnessMode == maxBrightnessMode) {
-				brightnessMode = 0;
-			}
-			else {
-				brightnessMode += 1;
-			}
-		}
-		if(curPin >= 9 && curPin <= 14) { 
-			// left - prev brightness mode
-
-			if(brightnessMode == 0) {
-				brightnessMode = maxBrightnessMode;
-			}
-			else {
-				brightnessMode -= 1;
-			}
-		}
-		if(curPin >= 22 && curPin <= 27) { 
-			// up - next color mode
-
-			if(colorMode == maxColorMode) {
-				colorMode = 0;
-			}
-			else {
-				colorMode += 1;
-			}
-		}
-		if(curPin >= 47 && curPin <= NUMPIXELS || curPin >= 0 && curPin <= 2) { 
-			// down - prev color mode
-
-			if(colorMode == 0) {
-				colorMode = maxColorMode;
-			}
-			else {
-				colorMode -= 1;
-			}
-		}
-		return;
+uint8_t circleDistanceToPin(uint8_t first, uint8_t second) {
+	if(second > first) {
+		return(second - first);
 	}
+	else {
+		return(NUMPIXELS - 1 - first + second);
+	}
+}
 
-	if(modeButtonStatus = HIGH) {
-		prevModeButtonStatus = HIGH;
-		return;
+uint8_t pinWithOffset(uint8_t pin, uint8_t offset) {
+	if(pin + offset < (NUMPIXELS - 1)) {
+		return(pin + offset);
+	}
+	else {
+		uint8_t overlap = ((NUMPIXELS - 1) - pin);
+		return (offset - overlap);
+	}
+}
+
+void setColorMapBasedOnPin(uint8_t pin) {
+
+	switch(colorMode) {
+		case 0:
+			//red
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				colorMap[i][0] = 1.0;
+				colorMap[i][1] = 0.0;
+				colorMap[i][2] = 0.0;
+			}
+			break;
+		case 1:
+
+			// blue and red
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				if(i%2 == 0) {
+					colorMap[i][0] = 1.0;
+					colorMap[i][1] = 0.0;
+					colorMap[i][2] = 0.0;
+				}
+				else {
+					colorMap[i][0] = 0.0;
+					colorMap[i][1] = 0.0;
+					colorMap[i][2] = 1.0;
+				}
+			}
+			break;
+		case 2:
+			
+			// HSL color ring
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				float H = ((float)i/(NUMPIXELS-1))*360.0;
+				struct HSL data = {H, 1.0, 0.5};
+				struct RGB value = HSLToRGB(data);
+				colorMap[i][0] = (uint8_t)value.R/255;
+				colorMap[i][1] = (uint8_t)value.G/255;
+				colorMap[i][2] = (uint8_t)value.B/255;
+			}
+			break;
+
+		case 3:
+			// 3 moving color areas
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				uint8_t dis = circleDistanceToPin(i, pin);
+				if(dis >= NUMPIXELS/6 && dis < 3*(NUMPIXELS/6)) {
+					colorMap[i][0] = 1.0;
+					colorMap[i][1] = 0.0;
+					colorMap[i][2] = 0.0;
+				}
+				if(dis >= 3*(NUMPIXELS/6) && dis < 5*(NUMPIXELS/6)) {
+					colorMap[i][0] = 0.0;
+					colorMap[i][1] = 1.0;
+					colorMap[i][2] = 0.0;
+				}
+				if((dis >= 5*(NUMPIXELS/6) && dis < NUMPIXELS) || dis < NUMPIXELS/6) {
+					colorMap[i][0] = 0.0;
+					colorMap[i][1] = 0.0;
+					colorMap[i][2] = 1.0;
+				}
+			}
+			break;
+
+		case 4:
+			//red green blue in succession
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				uint8_t diff = pin%3;
+				switch(diff) {
+					case 0:
+						colorMap[i][0] = 1.0;
+						colorMap[i][1] = 0.0;
+						colorMap[i][2] = 0.0;
+						break;
+					case 1:
+						colorMap[i][0] = 0.0;
+						colorMap[i][1] = 1.0;
+						colorMap[i][2] = 0.0;
+						break;
+					case 2:
+						colorMap[i][0] = 0.0;
+						colorMap[i][1] = 0.0;
+						colorMap[i][2] = 1.0;
+						break;
+				}
+			}
+			break;
+
+		case 5:
+			//red*2 green*2 blue*2 static
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				uint8_t diff = i%6;
+				switch(diff) {
+					case 0:
+					case 1:
+						colorMap[i][0] = 1.0;
+						colorMap[i][1] = 0.0;
+						colorMap[i][2] = 0.0;
+						break;
+					case 2:
+					case 3:
+						colorMap[i][0] = 0.0;
+						colorMap[i][1] = 1.0;
+						colorMap[i][2] = 0.0;
+						break;
+					case 4:
+					case 5:
+						colorMap[i][0] = 0.0;
+						colorMap[i][1] = 0.0;
+						colorMap[i][2] = 1.0;
+						break;
+				}
+			}
+			break;
+		//adjust maxColorMode when adding modes and don't forget break..
 	}
 }
 
@@ -334,37 +333,128 @@ void setBrightnessMapBasedOnPin(uint8_t pin) {
 			}
 			break;
 		case 1:
-			//scattered dots equal distance
+			// 1 heap of brightness
 			for(uint8_t i=0; i<NUMPIXELS; ++i) {
-				brightnessMap[i] = 0;
-			}
-			if(pin != INT8_MAX){
-				brightnessMap[pin] = 200;
+
+				uint16_t penalty = distanceToPin(i, pin)*30;
+
+				if(penalty > 250) {
+					brightnessMap[i] = 0;
+				}
+				else {
+					brightnessMap[i] = (uint8_t)(0.5*(250 - penalty));
+				}
 			}
 			break;
 		case 2:
-			//Decreasing brightness across circle
+			// 3 heaps of brightness
 			for(uint8_t i=0; i<NUMPIXELS; ++i) {
-				uint8_t d = distanceToPin(i, pin);
-				uint8_t val = (uint8_t)(((float)d/(float)NUMPIXELS)*500.0);
-				//ardprintf("i: %d, pin: %d, dist: %d, val: %d", i, pin, d, val);
-				brightnessMap[i] = val;//(uint8_t)(((float)d/(float)NUMPIXELS)*250.0);
+				
+				uint8_t dis = min(min(
+							distanceToPin(i, pin), 
+							distanceToPin(i, pinWithOffset(pin, NUMPIXELS/3))),
+							distanceToPin(i, pinWithOffset(pin, 2*(NUMPIXELS/3))));
+							
+				uint16_t penalty = dis*50;
+
+				if(penalty > 250) {
+					brightnessMap[i] = 0;
+				}
+				else {
+					brightnessMap[i] = (uint8_t)(0.5*(250 - penalty));
+				}
 			}
 			break;
 		case 3:
-			//every 10 leds full
+			// every 10 leds full
 			for(uint8_t i=0; i<NUMPIXELS; ++i) {
 				if(i%10 == pin%10) {
-					brightnessMap[i] = 250;
+					brightnessMap[i] = 200;
 				}
 				else {
 					brightnessMap[i] = 0;
 				}
 			}
 			break;
+		case 4:
+			// decreasing brightness across circle
+			for(uint8_t i=0; i<NUMPIXELS; ++i) {
+				uint8_t d = distanceToPin(i, pin);
+				uint8_t val = (uint8_t)(((float)d/(float)NUMPIXELS)*180.0);
+				//ardprintf("i: %d, pin: %d, dist: %d, val: %d", i, pin, d, val);
+				brightnessMap[i] = val;//(uint8_t)(((float)d/(float)NUMPIXELS)*250.0);
+			}
+			break;
+
 		//adjust maxBrightnessMode when adding modes
 	}
 }
+
+
+void switchMode(uint8_t curPin) {
+
+	if(curPin >= 31 && curPin <= 41) { 
+		// right - next brightness mode
+		Serial.println("right - next brightness mode");
+
+		if(brightnessMode == maxBrightnessMode) {
+			brightnessMode = 0;
+		}
+		else {
+			brightnessMode += 1;
+		}
+	}
+	if(curPin >= 7 && curPin <= 16) { 
+		// left - prev brightness mode
+		Serial.println("left - prev brightness mode");
+
+		if(brightnessMode == 0) {
+			brightnessMode = maxBrightnessMode;
+		}
+		else {
+			brightnessMode -= 1;
+		}
+	}
+	if(curPin >= 20 && curPin <= 29) { 
+		// up - next color mode
+		Serial.println("up - next color mode");
+
+		if(colorMode == maxColorMode) {
+			colorMode = 0;
+		}
+		else {
+			colorMode += 1;
+		}
+	}
+	if((curPin >= 45 && curPin <= NUMPIXELS) || (curPin >= 0 && curPin <= 4)) { 
+		// down - prev color mode
+		Serial.println("down - prev color mode");
+
+		if(colorMode == 0) {
+			colorMode = maxColorMode;
+		}
+		else {
+			colorMode -= 1;
+		}
+	}
+}
+
+void checkModeButton() {
+	modeButtonStatus = digitalRead(MODE_BUTTON_PIN);
+
+	if(modeButtonStatus == LOW && prevModeButtonStatus == HIGH) {
+		prevModeButtonStatus = LOW;
+		switchMode(getCurrentPin(true));
+
+		return;
+	}
+
+	if(modeButtonStatus == HIGH) {
+		prevModeButtonStatus = HIGH;
+		return;
+	}
+}
+
 
 void setPinOneStep(uint8_t pin) {
 	//ardprintf("Setting pin %d for duration %d", pin, steps);
@@ -372,14 +462,11 @@ void setPinOneStep(uint8_t pin) {
 	setBrightnessMapBasedOnPin(pin);
 
 	for(int i=0; i<NUMPIXELS; ++i) {
-		pixels.setPixelColor(i, pixels.Color(colorMap[i][0]*brightnessMap[i],
-					colorMap[i][1]*brightnessMap[i],
-					colorMap[i][2]*brightnessMap[i]));
+		pixels.setPixelColor(i, pixels.Color((uint8_t)(colorMap[i][0]*brightnessMap[i]),
+					(uint8_t)(colorMap[i][1]*brightnessMap[i]),
+					(uint8_t)(colorMap[i][2]*brightnessMap[i])));
 	}
 	pixels.show();
-
-	//Serial.println("PIN playback");
-	//Serial.println(pin);
 
 	delay(STEP_DELAY);
 }
@@ -452,7 +539,7 @@ void recordSequence() {
 			continue;
 		}
 
-		uint8_t curPin = getCurrentPin();
+		uint8_t curPin = getCurrentPin(true);
 		setPinOneStep(curPin);
 
 		if(curPin == prevPin) {
@@ -478,7 +565,7 @@ void loop() {
 	switch(mode) {
 		case IDLE:
 			uint8_t pin;
-			pin = getCurrentPin();
+			pin = getCurrentPin(true);
 			setPinOneStep(pin);
 
 			checkModeButton();
